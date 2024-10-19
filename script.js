38fecd90a91ef3534a1156969528fc6b6d953751
Vina-permatasari <vinapermatasari670@gmail.com>
2024-10-19 21:50:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.hire__me').addEventListener('click', function()) {
+    alert("Thank you for considering to hire me!");
+};
+
+document.querySelector('.portofolio').addEventListener('click' , function()) {
+    
+    alert("Here is my portfolio!");
+};
